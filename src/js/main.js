d1d443c766356b3ac424f52de768a37f7de4eadd
Gaoussou Koné <gaoussou.ony@gmail.com>
2018-02-05 00:44:49 +0100

    adding windowResize

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,8 @@ import { log } from 'three';
 
 const OrbitControls = require('three-orbit-controls')(THREE)
 var VARS = {
-  message : 'Olympics',
-  speed : .005,
+  message: 'Olympics',
+  speed: .005,
 }
 
 class Main {
@@ -24,21 +24,21 @@ class Main {
     this.camera = new THREE.PerspectiveCamera(30, data.w / data.h, 1, 10000);
     this.camera.position.z = data.distance
 
-    // this.controls = new OrbitControls(this.camera)
-
     // DAT.GUI : https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
     gui.add(VARS, 'message');
     gui.add(VARS, 'speed', -.2, .2);
 
-    var container = document.getElementById("container")
+    this.container = document.getElementById("container")
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setSize(data.w, data.h);
 
     this.renderer.domElement.style.position = 'absolute';
-    container.appendChild(this.renderer.domElement);
-    this.controls = new OrbitControls(this.camera) // this.controls = new OrbitControls(this.camera)
+    this.container.appendChild(this.renderer.domElement);
+    // this.controls = new OrbitControls(this.camera) // this.controls = new OrbitControls(this.camera)
     this.animate = this.animate.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
+    console.log(this);
 
   }
 
@@ -49,20 +49,25 @@ class Main {
   }
 
   render() {
-    data.distance += (data.distanceTarget - data.distance ) * .3;
-    if(data.distance <= 1001) {
+    data.distance += (data.distanceTarget - data.distance) * .3;
+    if (data.distance <= 1001) {
       data.distance = data.distanceTarget;
     }
     this.camera.position.z = data.distance;
     // console.log((data.distanceTarget - data.distance) * 0.3)
-    this.Globe.mesh.rotation.y += VARS.speed 
-
-
-
+    this.Globe.mesh.rotation.y += VARS.speed
     this.camera.lookAt(this.Globe.mesh.position);
     this.renderer.render(this.Decor.scene, this.camera);
+  }
+
+  onWindowResize(event) {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
   }
 }
 
 var app = new Main()
 app.animate()
+
+window.addEventListener('resize', app.onWindowResize, false);
