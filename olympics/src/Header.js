8647e885c82c30a522449351a934084061f62ffa
Johanna Khenkittisak <johanna.khen@gmail.com>
2018-02-20 18:47:20 +0100

    Fix missing header tag

--- olympics/src/Header.js
+++ olympics/src/Header.js
@@ -4,12 +4,14 @@ import './css/header.scss';
 class Header extends Component {
   render() {
     return (
+      <header class="header">
       <section className="wrapper wrapper__medium">
         <a href="#no-link" className="header__more">About</a>
         <div className="header__logo-wrapper">
           <a href="#linkToHome" className="header__logo">OUAG</a>
         </div>
       </section>
+      </header>
     );
   }
 }
