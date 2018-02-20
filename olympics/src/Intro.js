2eb87c47bfcd8420d7fe104e95ee2aa0996347ec
Johanna Khenkittisak <johanna.khen@gmail.com>
2018-02-20 18:42:46 +0100

    Add gsap

--- olympics/src/Intro.js
+++ olympics/src/Intro.js
@@ -1,17 +1,41 @@
+import './js/split.js';
 import React, { Component } from 'react';
+import Header from './Header';
+import './css/header.scss';
 import './css/intro.scss';
+import {TimelineLite,TweenLite} from 'gsap';
+import SplitText from './js/split';
 
 class Intro extends Component {
+  constructor (){
+    super()
+    this.animate = this.animate;
+  }
+  animate(){
+    var tl = new TimelineLite(),
+    mySplitText = new SplitText(".intro__text", {type:"words,chars"}),
+    chars = mySplitText.chars; //an array of all the divs that wrap each character
+
+  TweenLite.set(".intro__text", {perspective:400});
+
+  tl.staggerFrom(chars, 0.8, {opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50"}, 0.01, "+=0");
+
+  }
   render() {
     return (
-      <section className="intro">
-        <div className="overlay"></div>
-        <div className="intro__wrapper center">
-          <p className="intro__text">At the outset, a sporting event is a gathering of persons sharing the same passion.
-          What is supposed to be so neutral can be triggering real socio-political leverages.
-          </p>
-        </div>
-      </section>
+      <div>
+        <Header></Header>
+        <section className="intro">
+          <div className="overlay">
+          {/* <button onClick={this.animate}>Activate Lasers</button> */}
+          </div>
+          <div className="intro__wrapper center">
+            <p className="intro__text">At the outset, a sporting event is a gathering of persons sharing the same passion.
+            What is supposed to be so neutral can be triggering real socio-political leverages.
+            </p>
+          </div>
+        </section>
+      </div>
     );
   }
 }
--- olympics/src/index.js
+++ olympics/src/index.js
@@ -2,8 +2,6 @@ import './css/normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './Header';
 import Intro from './Intro';
 
-ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Intro />, document.getElementById('root'));
