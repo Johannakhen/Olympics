4439d558b22204e57bfaa99b01c3f734cfbb5d5b
Gaoussou Koné <gaoussou.ony@gmail.com>
2018-02-23 10:17:47 +0100

    Panel componnent

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -3,6 +3,8 @@ import {TimelineLite,TweenLite} from 'gsap';
 import SplitText from '../libs/SplitText';
 import Path from './path';
 import Main from '../main'
+import Panel from './Panel'
+
 // import sound from './img/sound.svg';
 // import soundBars from './img/sound-bars.svg';
 
@@ -59,17 +61,7 @@ export default class App extends Component {
             <li><a href="">Ouag</a></li> 
           </ul>
         </div>
-        <div id="panel">
-          <div id="panel__wrapper">
-            <div className="align__left">about the project</div>
-            <div className="align__right">
-              <strong>Once Upon A Game aka OUAG, is an interactive website born from a school project which tells the story of the Olympic Games from a socio-political point of view.</strong>
-              <p>Through several milestones such as the Games of Antwerp (1920), Berlin (1936), Melbourne (1956), Moscow (1980), Mexico (1968) or Montreal (1976), we tried to demonstrate the following problem: "How does a sporting event exacerbate and expose tensions within the organizing country and in the world? ".
-              In order to achieve this, we carried out a long journalistic work that we transcribed and synthesized while mixing it with a dark graphic aspect.</p>
-              <small>6 month project, 5 contributors: Johanna Khen, Gaoussou Koné, Florian Planchenault, Jade Mihami, Tiffanie Train.</small>
-            </div>
-          </div>
-        </div>
+        <Panel/>
         <div className="app" id="intro" onLoad={this.animate}>
           <div className="app__intro" >
             <p className="app__intro--text">At the outset, a sporting event is a gathering of persons sharing the same passion.
