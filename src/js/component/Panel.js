42ebe69f534bb00dede34f22864927ae0562763a
Johanna Khenkittisak <johanna.khen@gmail.com>
2018-02-27 17:06:48 +0100

    Wrap inside align right img

--- src/js/component/Panel.js
+++ src/js/component/Panel.js
@@ -4,9 +4,9 @@ import Photo from '../../img/photo1.png'
 const Panel = () => (
 	<div id="panel">
 		<div id="panel__wrapper">
-			<img src={Photo}/>
 			<div className="align__left">about the project</div>
 				<div className="align__right">
+				<img src={Photo}/>
 				<strong>Once Upon A Game aka OUAG, is an interactive website born from a school project which tells the story of the Olympic Games from a socio-political point of view.</strong>
 				<p>Through several milestones such as the Games of Antwerp (1920), Berlin (1936), Melbourne (1956), Moscow (1980), Mexico (1968) or Montreal (1976), we tried to demonstrate the following problem: "How does a sporting event exacerbate and expose tensions within the organizing country and in the world? ".
 				In order to achieve this, we carried out a long journalistic work that we transcribed and synthesized while mixing it with a dark graphic aspect.</p>
